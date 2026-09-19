ca00d4b04b63d67109ff11f19a9998498418b8ab
TusharVisave <tusharvisave2005@gmail.com>
2026-09-19 22:40:50 +0530

    test: add first Linked List DSA problem

---
+++ src/main/java/com/Tushar/dsajava/linkedlist/reverselinkedlist/Solution.java
@@ -0,0 +1,22 @@
+package com.Tushar.dsajava.linkedlist.reverselinkedlist;
+
+public class Solution {
+
+    public ListNode reverseList(ListNode head) {
+
+        ListNode previous = null;
+        ListNode current = head;
+
+        while (current != null) {
+
+            ListNode next = current.next;
+
+            current.next = previous;
+
+            previous = current;
+            current = next;
+        }
+
+        return previous;
+    }
+}
